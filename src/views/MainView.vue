99b159d06dedb5e6caae0765f658036b37917c22
JohansonCda <jcv9806@hotmail.com>
2025-05-09 22:57:51 -0500

    create routes and order the Main view

---
+++ src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/components/Sidebar.vue';
+import { RouterView } from 'vue-router';
+</script>´
+
+<style>
+.container {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
