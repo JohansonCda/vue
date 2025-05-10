866aa776bbce55353a09707cc646def1f2abd811
JohansonCda <jcv9806@hotmail.com>
2025-05-09 22:31:50 -0500

    Create form with list, add tasks function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Contador></Contador>
+  <Lista></Lista>
 </template>
 
 <script setup>
 import Contador from './components/Contador.vue';
+import Lista from './components/ListaDeTareas.vue';
 </script>
 
 <style scoped></style>
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Lista de tareas</h2>
+    <input type="text" v-model="nuevaTarea" @keyup.enter="agregarTarea">
+    <ul>
+      <li v-for="(tarea, index) in tareas" :key="index">
+        {{ tarea }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const agregarTarea = () => {
+  tareas.value.push(nuevaTarea.value);
+  nuevaTarea.value = '';
+}
+
+</script>
+
+<style></style>
