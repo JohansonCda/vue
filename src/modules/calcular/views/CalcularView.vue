92770000b59d96c3a8766ca15afbc1d0599d91dd
JohansonCda <jcv9806@hotmail.com>
2025-05-14 21:05:58 -0500

    Show, if, else if

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -1,7 +1,9 @@
 <template>
   <h3>Porpiedades comutadas</h3>
   <input type="text" v-model="valor">
-  <ResultadoSection :resultado="valorCalculado"></ResultadoSection>
+  <ResultadoSection v-if="mostrarElemento" :resultado="valorCalculado"></ResultadoSection>
+  <div v-else-if="isNaN(valor)">No letras</div>
+  <div v-else>Aqui no se muestra nada</div>
 </template>
 
 <script setup>
@@ -11,6 +13,10 @@ import ResultadoSection from '../components/ResultadoSection.vue';
 const valor = ref('');
 const valorCalculado = computed(()=>{
   return valor.value * 3;
+});
+
+const mostrarElemento = computed(()=>{
+  return valor.value > 10;
 });
 
 </script>
